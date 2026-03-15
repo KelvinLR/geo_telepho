7355498b249c0c6c930697d756f1a4b72b0145c8
Kelvin <kelvindelima710@gmail.com>
2026-03-15 17:21:36 -0300

    feat: create MapPage widget

---
+++ lib/ui/views/map_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:geo_telepho/data/controllers/map_controllermvc.dart';
+import 'package:latlong2/latlong.dart';
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  List<Polygon> statePolygons = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadStates();
+  }
+
+  void loadStates() async {
+    final controller = MapControllerMVC();
+
+    final polygons = await controller.loadBrazilStates();
+
+    setState(() {
+      statePolygons = polygons;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Map Page')),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(-3.71839, -38.5434),
+
+          initialZoom: 12,
+          minZoom: 3,
+          maxZoom: 18,
+          interactionOptions: const InteractionOptions(
+            flags:
+                InteractiveFlag.pinchZoom |
+                InteractiveFlag.doubleTapZoom |
+                InteractiveFlag.drag,
+          ),
+        ),
+        children: [
+          TileLayer(
+            urlTemplate:
+                'https://basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+            userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+          ),
+          PolygonLayer(polygons: statePolygons),
+        ],
+      ),
+    );
+  }
+}
